3cef175b6b85d2ee2a65d8735260eb31feac4379
sinitcin <antony@email.su>
2021-09-28 10:08:57 +0300

    Removed unnecessary metadata
    Signed-off-by: sinitcin <antony@email.su>

--- pallets/utxo/src/lib.rs
+++ pallets/utxo/src/lib.rs
@@ -341,9 +341,8 @@ pub mod pallet {
 
     #[pallet::event]
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
-    #[pallet::metadata(T::AccountId = "AccountId", T::AssetId = "AssetId")]
+    #[pallet::metadata(T::AccountId = "AccountId")]
     pub enum Event<T: Config> {
-        /// Some asset class was created. \[asset_id, creator and owner\]
         TokenCreated(u32, T::AccountId),
         TransactionSuccess(TransactionFor<T>),
     }
