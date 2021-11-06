0a2bf4f264d20c05aa8711f3367840bfe925f3b7
Aaro Altonen <aaro.altonen@mintlayer.org>
2021-11-06 09:05:25 +0200

    tests: Add functional tests for Destination::FundPP

--- test/functional/feature_smart_contract_test.py
+++ test/functional/feature_smart_contract_test.py
@@ -24,20 +24,18 @@ from test_framework.util import (
 import os
 
 # helper function to reduce code duplication
-def submit_pp_tx(client, input_utxo, alice, value, output):
+def submit_pp_tx(client, input_utxo, alice, value, outputs):
+    outputs.insert(0, utxo.Output(
+        value=value,
+        header=0,
+        destination=utxo.DestPubkey(alice.public_key)
+    ))
     tx = utxo.Transaction(
         client,
         inputs=[
             utxo.Input(input_utxo.outpoint(0)),
         ],
-        outputs=[
-            utxo.Output(
-                value=value,
-                header=0,
-                destination=utxo.DestPubkey(alice.public_key)
-            ),
-            output
-        ]
+        outputs = outputs,
     ).sign(alice, [input_utxo.outputs[0]], [0])
     return tx, client.submit(alice, tx)
 
@@ -102,38 +100,38 @@ class ExampleTest(MintlayerTestFramework):
         # invalid bytecode
         value -= 1
 
-        (tx, (ext, blk)) = submit_pp_tx(client, tx, alice, value, utxo.Output(
+        (tx, (ext, blk)) = submit_pp_tx(client, tx, alice, value, [utxo.Output(
             value=1,
             header=0,
             destination=utxo.DestCreatePP(
                 code=[0x00],
                 data=[0xed, 0x4b, 0x9d, 0x1b],
             )
-        ))
+        )])
         assert_equal(contract.getContractAddresses(substrate, blk), None)
 
         # invalid value
-        (invalid_tx, res) = submit_pp_tx(client, tx, alice, value, utxo.Output(
+        (invalid_tx, res) = submit_pp_tx(client, tx, alice, value, [utxo.Output(
             value=0,
             header=0,
             destination=utxo.DestCreatePP(
                 code=os.path.join(os.path.dirname(__file__), "assets/pooltester.wasm"),
                 data=[0xed, 0x4b, 0x9d, 0x1b],
             )
-        ))
+        )])
         assert_equal(res, None)
 
         # valid data
         value -= 1
 
-        (tx, (ext, blk)) = submit_pp_tx(client, tx, alice, value, utxo.Output(
+        (tx, (ext, blk)) = submit_pp_tx(client, tx, alice, value, [utxo.Output(
             value=1,
             header=0,
             destination=utxo.DestCreatePP(
                 code=os.path.join(os.path.dirname(__file__), "assets/pooltester.wasm"),
-                data=[0xed, 0x4b, 0x9d, 0x1b],
+                data=[0x9b, 0xae, 0x9d, 0x5e],
             )
-        ))
+        )])
 
         (ss58, acc_id) = contract.getContractAddresses(substrate, blk)
         contractInstance = contract.ContractInstance(
@@ -150,78 +148,141 @@ class ExampleTest(MintlayerTestFramework):
         value -= 1
         msg_data = contractInstance.generate_message_data("flip", {})
 
-        (tx, (ext, blk)) = submit_pp_tx(client, tx, alice, value, utxo.Output(
+        (tx, (ext, blk)) = submit_pp_tx(client, tx, alice, value, [utxo.Output(
             value=1,
             header=0,
             destination=utxo.DestCallPP(
                 dest_account=acc_id,
-                fund=False,
                 input_data=bytes.fromhex(msg_data.to_hex()[2:]),
             )
-        ))
+        )])
         result = contractInstance.read(alice, "get")
         assert_equal(result.contract_result_data.value, -1337)
 
         # invalid `value` given
         msg_data = contractInstance.generate_message_data("flip", {})
 
-        (invalid_tx, res) = submit_pp_tx(client, tx, alice, value, utxo.Output(
+        (invalid_tx, res) = submit_pp_tx(client, tx, alice, value, [utxo.Output(
             value=0,
             header=0,
             destination=utxo.DestCallPP(
                 dest_account=alice.public_key,
-                fund=False,
                 input_data=bytes.fromhex(msg_data.to_hex()[2:]),
             )
-        ))
+        )])
         assert_equal(res, None)
 
-        # test contract-to-p2k transfer from alice to bob
+        # query the initial value of the contract
         #
-        # `send_to_pubkey()` first funds the smart contract from alice's funds
-        # and when the wasm code is executed, the funds are transferred to bob
-        msg_data = contractInstance.generate_message_data("send_to_pubkey", { "dest": bob.public_key })
+        # each successful tranfser will update the value by one
+        # and each call that fails doesn't change the value
+        result = contractInstance.read(alice, "get")
+        assert_equal(result.contract_result_data.value, -1337)
+
+        # try to call contract without funding it
+        msg_data = contractInstance.generate_message_data("send_to_pubkey", { "dest": bob.public_key, "value": 555 })
         value -= 555
 
-        (tx, (ext, blk)) = submit_pp_tx(client, tx, alice, value, utxo.Output(
+        (tx, (ext, blk)) = submit_pp_tx(client, tx, alice, value, [utxo.Output(
             value = 555,
             header = 0,
             destination = utxo.DestCallPP(
                 dest_account = acc_id,
-                fund = True,
                 input_data = bytes.fromhex(msg_data.to_hex()[2:]),
             )
-        ))
+        )])
 
-        # verify that bob actually received the utxo
-        bobs_utxos = [x for x in client.utxos_for(bob)]
-        assert_equal(len(bobs_utxos), 1)
-        assert_equal(bobs_utxos[0][1].json()['value'], 555)
+        # call failed, the value is not updated
+        # assert_equal(get_state_var(contractInstance, client, alice), -1337)
 
-        # test contract-to-p2pk again but this time don't fund the contract
-        # meaning that after the TX, bob only has the UTXO he received in the previous test case
-        # and the contract has a UTXO with value 666
-        msg_data = contractInstance.generate_message_data("send_to_pubkey", { "dest": bob.public_key })
-        value -= 666
+        # fund the contract (but not enough) and call it
+        msg_data = contractInstance.generate_message_data("send_to_pubkey", { "dest": bob.public_key, "value": 500 })
+        value -= 500
 
-        (tx, (ext, blk)) = submit_pp_tx(client, tx, alice, value, utxo.Output(
-            value = 666,
-            header = 0,
-            destination = utxo.DestCallPP(
-                dest_account = acc_id,
-                fund = False,
-                input_data = bytes.fromhex(msg_data.to_hex()[2:]),
-            )
-        ))
+        (tx, (ext, blk)) = submit_pp_tx(client, tx, alice, value, [
+            utxo.Output(
+                value = 400,
+                header = 0,
+                destination = utxo.DestFundPP(acc_id)
+            ),
+            utxo.Output(
+                value = 100,
+                header = 0,
+                destination = utxo.DestCallPP(
+                    dest_account = acc_id,
+                    input_data = bytes.fromhex(msg_data.to_hex()[2:]),
+                )
+            ),
+        ])
 
-        # verify that bob still has only one UTXO
-        utxos = [x for x in client.utxos_for(bob)]
-        assert_equal(len(utxos), 1)
+        # call failed, the value is not updated
+        result = contractInstance.read(alice, "get")
+        assert_equal(result.contract_result_data.value, -1337)
 
-        # verify that the contract has one utxo with value 666
-        utxos = [x for x in client.utxos_for(acc_id[2:])]
-        assert_equal(len(utxos), 1)
-        assert_equal(utxos[0][1].json()["value"], 666)
+        """ Fund the contract and call it """
+        msg_data = contractInstance.generate_message_data("send_to_pubkey", { "dest": bob.public_key, "value": 500 })
+        value -= 200
+
+        (tx, (ext, blk)) = submit_pp_tx(client, tx, alice, value, [
+            utxo.Output(
+                value = 100,
+                header = 0,
+                destination = utxo.DestFundPP(acc_id)
+            ),
+            utxo.Output(
+                value = 100,
+                header = 0,
+                destination = utxo.DestCallPP(
+                    dest_account = acc_id,
+                    input_data = bytes.fromhex(msg_data.to_hex()[2:]),
+                )
+            ),
+        ])
+
+        # call succeeded, the value is updated
+        result = contractInstance.read(alice, "get")
+        assert_equal(result.contract_result_data.value, -1336)
+
+        # verify that Bob has 1 UTXO with value 500
+        bobs = [x for x in client.utxos_for(bob.public_key)]
+        assert_equal(len(bobs), 1)
+        assert_equal(bobs[0][1].json()["value"], 500)
+
+        # verify that the contract only has CallPP UTXOs
+        contract_utxos = [x for x in client.utxos_for(acc_id[2:])]
+        callpp_utxos = [x for x in contract_utxos if list(x[1].json()["destination"])[0] == "CallPP"]
+        assert_equal(len(contract_utxos), len(callpp_utxos))
+
+        """ Fund the contract and call it but don't transfer all of the funds """
+        msg_data = contractInstance.generate_message_data("send_to_pubkey", { "dest": bob.public_key, "value": 200 })
+        value -= 600
+
+        (tx, (ext, blk)) = submit_pp_tx(client, tx, alice, value, [
+            utxo.Output(
+                value = 500,
+                header = 0,
+                destination = utxo.DestFundPP(acc_id)
+            ),
+            utxo.Output(
+                value = 100,
+                header = 0,
+                destination = utxo.DestCallPP(
+                    dest_account = acc_id,
+                    input_data = bytes.fromhex(msg_data.to_hex()[2:]),
+                )
+            ),
+        ])
+
+        # verify that bob has two UTXOs and that their total value is 700
+        bobs = [x for x in client.utxos_for(bob.public_key)]
+        total_value = sum([x[1].json()["value"] for x in bobs])
+        assert_equal(len(bobs), 2)
+        assert_equal(total_value, 700)
+
+        # verify that the contract has one FundPP UTXO with value 300
+        fundpps = [x for x in client.utxos_for(acc_id[2:]) if list(x[1].json()["destination"])[0] == "FundPP"]
+        assert_equal(len(fundpps), 1)
+        assert_equal(fundpps[0][1].json()["value"], 300)
 
         # try to call a contract that doesn't exist (alice's public key
         # doesn't point to a valid smart contract)
@@ -230,21 +291,19 @@ class ExampleTest(MintlayerTestFramework):
         # spent, i.e., if the UTXO set is queried, you'll find a UTXO
         # with value 888 meaning user just lost his money which is
         # not the correct behavior but the implementation is still under way
-        msg_data = contractInstance.generate_message_data("fund", {})
         value -= 888
 
-        (tx, (ext, blk)) = submit_pp_tx(client, tx, alice, value, utxo.Output(
+        (tx, (ext, blk)) = submit_pp_tx(client, tx, alice, value, [utxo.Output(
             value = 888,
             header = 0,
             destination = utxo.DestCallPP(
                 dest_account = alice.public_key,
-                fund = True,
-                input_data = bytes.fromhex(msg_data.to_hex()[2:]),
+                input_data = [0x00, 0x01, 0x02, 0x03],
             )
-        ))
+        )])
 
         result = contractInstance.read(alice, "get")
-        assert_equal(result.contract_result_data.value, -1337)
+        assert_equal(result.contract_result_data.value, -1335)
 
         # Test cross-contract calls
         #
@@ -258,14 +317,14 @@ class ExampleTest(MintlayerTestFramework):
         # When all that's done, query the value again and verify that it has been updated
         value -= 111
 
-        (tx, (ext, blk)) = submit_pp_tx(client, tx, alice, value, utxo.Output(
+        (tx, (ext, blk)) = submit_pp_tx(client, tx, alice, value, [utxo.Output(
             value = 111,
             header = 0,
             destination = utxo.DestCreatePP(
                 code = os.path.join(os.path.dirname(__file__), "assets/c2c_tester.wasm"),
                 data = [0xed, 0x4b, 0x9d, 0x1b],
             )
-        ))
+        )])
 
         (ss58_c2c, acc_id_c2c) = contract.getContractAddresses(substrate, blk)
         c2cInstance = contract.ContractInstance(
@@ -283,21 +342,30 @@ class ExampleTest(MintlayerTestFramework):
             "selector": "0xc6298215",
             "value": 999,
         })
-        value -= 222
+        value -= 600
 
-        (tx, (ext, blk)) = submit_pp_tx(client, tx, alice, value, utxo.Output(
-            value = 222,
-            header = 0,
-            destination = utxo.DestCallPP(
-                dest_account = acc_id,
-                fund = True,
-                input_data = bytes.fromhex(msg_data.to_hex()[2:]),
+        (tx, (ext, blk)) = submit_pp_tx(client, tx, alice, value, [
+            utxo.Output(
+                value = 500,
+                header = 0,
+                destination = utxo.DestFundPP(acc_id)
+            ),
+            utxo.Output(
+                value = 100,
+                header = 0,
+                destination = utxo.DestCallPP(
+                    dest_account = acc_id,
+                    input_data = bytes.fromhex(msg_data.to_hex()[2:]),
+                )
             )
-        ))
+        ])
 
         # verify that the call succeeded
         result = c2cInstance.read(alice, "get")
         assert_equal(result.contract_result_data.value, 999)
+
+        result = contractInstance.read(alice, "get")
+        assert_equal(result.contract_result_data.value, -1334)
 
         # Try to spend the funds of a contract
         #
@@ -313,44 +381,37 @@ class ExampleTest(MintlayerTestFramework):
         # from the runtime storage as the smart contract has not transferred
         # these funds, the outpoint hash is **not** found from the storage
         # and this TX is rejected as invalid
-        msg_data = contractInstance.generate_message_data("fund", {})
         value -= 555
 
-        (tx, (ext, blk)) = submit_pp_tx(client, tx, alice, value, utxo.Output(
+        (tx, (ext, blk)) = submit_pp_tx(client, tx, alice, value, [utxo.Output(
             value = 555,
             header = 0,
-            destination = utxo.DestCallPP(
-                dest_account = acc_id,
-                fund = True,
-                input_data = bytes.fromhex(msg_data.to_hex()[2:]),
-            )
-        ))
+            destination = utxo.DestFundPP(acc_id)
+        )])
 
-        result = contractInstance.read(alice, "get")
-        assert_equal(result.contract_result_data.value, 1338)
-
-        utxos = [x for x in client.utxos_for(acc_id[2:])]
-        assert_equal(len(utxos), 1)
-        assert_equal(utxos[0][1].json()["value"], 555)
+		# fetch the FundPP UTXO that was just sent
+        utxos = [x for x in client.utxos_for(acc_id[2:]) if list(x[1].json()["destination"])[0] == "FundPP"]
+        assert_equal(len(utxos), 2)
+        assert_equal(utxos[1][1].json()["value"], 555)
 
         invalid_tx = utxo.Transaction(
             client,
             inputs = [
-                utxo.Input(utxos[0][0]),
+                utxo.Input(utxos[1][0]),
             ],
             outputs = [
                 utxo.Output(
-                    value = 666,
+                    value = 555,
                     header = 0,
                     destination = utxo.DestPubkey(alice.public_key)
                 ),
             ]
         )
 
-        # size of the outpoint (32 bytes, 0x10) + the outpoint itself
-        # the outpoint in the witness field is valid but because the
-        # smart contract has not spent the funds, the TX is rejected
-        tx.inputs[0].witness = bytearray.fromhex("10" + str(utxos[0][0])[2:])
+        # # size of the outpoint (32 bytes, 0x10) + the outpoint itself
+        # # the outpoint in the witness field is valid but because the
+        # # smart contract has not spent the funds, the TX is rejected
+        tx.inputs[0].witness = bytearray.fromhex("10" + str(utxos[1][0])[2:])
         assert_equal(client.submit(alice, invalid_tx), None)
 
 if __name__ == '__main__':
