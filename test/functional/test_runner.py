772ccc139ae1aabdd98e62dc6555a1363c282e97
Roy Ben Abraham <roy.ben.abraham@gmail.com>
2021-11-16 15:00:14 +0700

    Try running just first three tests

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -54,9 +54,9 @@ TEST_EXIT_PASSED = 0
 TEST_EXIT_SKIPPED = 77
 
 BASE_SCRIPTS= [
-#    'example_test.py',
-#    'feature_alice_bob_test.py',
-#    'feature_staking_extra.py',
+     'example_test.py',
+     'feature_alice_bob_test.py',
+     'feature_staking_extra.py'
 #    'feature_staking_extra_not_validator.py',
 #    'feature_staking_extra_wrong_controller.py'
 ]
